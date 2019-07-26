c11490317875aefdbc97ec6c92b3b7d5d8452afc
sanyam <f20180372@pilani.bits-pilani.ac.in>
2019-07-26 23:29:25 +0530

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,13 @@
+#if the user session is active it will directly login the user without the need to login again and again
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+def login_required(f):
+    
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function

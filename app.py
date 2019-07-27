be520a35f6ff90c3b7466c4af62c8c925fcadf07
sanyam <f20180372@pilani.bits-pilani.ac.in>
2019-07-27 10:32:26 +0530

    comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#importing default/required libraries
 import os, json
 import hashlib
 import datetime 
@@ -13,10 +14,8 @@ from helpers import login_required
 app = Flask(__name__)
 
 
-#DATABASE_URL='postgres://jimvhxxmxrmuxm:5366d987f5ae273c9e2ff11b57287858e63a6cd2ffbf6d7e856f4462095387c1@ec2-54-247-170-5.eu-west-1.compute.amazonaws.com:5432/dbm85gcandi1n8'
-# Check for environment variable
 if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+    raise RuntimeError("db_url is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -27,115 +26,125 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/refresh")
-def refresh():
-    return render_template("index.html")
+#buy new services/products 
+@app.route("/buy")
+@login_required
+def buy():
+    temp=db.execute("SELECT id,name,pricing,dd,mm,dept,contact,link,brief FROM product")
+    products = temp.fetchall()
+    
+    return render_template("buy.html",products=products)
 
+#redirects directly to the login page if its session is active
 @app.route("/")
 @login_required
 def index():
-#redirects directly to the login page if its session is active
     total=0
+    #select entries from main table
     rows = db.execute("SELECT  product_id,cts FROM main where user_id = :id AND cancel=0",{"id": session["user_id"]}).fetchall()
+    
+    #total expenditure
     for i in rows :
         total=total+int(i.cts)
+    
+    #empty list to store queried data
     names=[]
     dd=[]
     mm=[]
     cancel=[]
     product_id=[]
     
+    #selecting the corresponding products details 
     for row in rows:
         temp=db.execute("SELECT name,dd,mm,id FROM product where id = :id",{"id":row.product_id }).fetchone()
-        names.append(str(temp.name))
+        names.append(temp.name)
         dd.append(int(temp.dd))
         mm.append(int(temp.mm))
-        product_id.append(int(temp.id))
+        product_id.append((temp.id))
+
+        #checking that the cancellation date is passed or NOT
         canc = datetime.datetime(2019,int(temp.mm),int(temp.dd),23,59,59) 
         today = datetime.datetime.now()
-        
         if canc>=today:
             cancel.append(True)
         else:
             cancel.append(False)
-        
+    
+    #zipping all data to pass to Jinja 2.0
     info = zip(rows, names,dd,mm,cancel,product_id)
+    
     if rows==None:
-        return render_template("inedx.html",message="No records exist")
+        return render_template("inedx.html",message="No Transactions Done Yet")
+    
     return render_template("index.html",info=info,total=total)
 
+
+#function to cancel a product/service order pid--refres to product
 @app.route("/cancel/<int:pid>",methods=["GET"])
 @login_required
 def cancel(pid):
     
-    #pid=int(request.form.get("pid"))
     db.execute("UPDATE main SET cancel=:true WHERE user_id= :user_id AND product_id=:product_id;",
         {"true":int(1),"user_id": session["user_id"],"product_id":pid})
     db.commit()
     
     return redirect("/")
 
-
+#login page (authenticating user)
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """ Log user in """
-
-    # Forget any user_id
+    #Forget any previous user_id
     session.clear()
 
     username = request.form.get("username")
     password=request.form.get("password")
-    # User reached route via POST (as by submitting a form via POST)
+    #user reached route via POST 
     if request.method == "POST":
 
-        # Ensure username was submitted
+        #ensure username was submitted
         if not request.form.get("username"):
             return render_template("error.html", message="must provide username")
 
-        # Ensure password was submitted
+        #ensure password was submitted
         elif not request.form.get("password"):
             return render_template("error.html", message="must provide password")
 
-        # Query database for username (http://zetcode.com/db/sqlalchemy/rawsql/)
-        # https://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.ResultProxy
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                             {"username": username})
         
         result = rows.fetchone()
 
-        # Ensure username exists and password is correct
+        #ensure username exists and password is correct
         if result == None : 
-            return render_template("error.html", message="invalid username")
+            return render_template("error.html", message="invalid username or password")
+        
+        #password are stored in hashed form for security reasons
+        #this is a one-way encryption 
         password=password.encode()
         hash_object = hashlib.md5(password)
         password=hash_object.hexdigest()
+        
         if result[2]!=password :
-            return render_template("error.html", message="invalid  password")
+            return render_template("error.html", message="invalid username or password")
 
-        # Remember which user has logged in
+        #remember which user has logged in
         session["user_id"] = result[0]
         session["user_name"] = result[1]
 
-        #loading users financial records
-        rows = db.execute("SELECT user_id FROM main WHERE user_id = :username",
-                            {"username": result[0]}).fetchone
-        if rows==None :
-            return render_template("error.html")
-        # Redirect user to home page
+        #redirect user to home page
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
+    #user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
 @app.route("/logout")
 def logout():
-    """ Log user out """
-
-    # Forget any user ID
+    
+    #forget any user ID
     session.clear()
 
-    # Redirect user to login form
+    #redirect user to login form
     return redirect("/")
 
 
